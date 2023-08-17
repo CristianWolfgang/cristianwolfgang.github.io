42fb4204ad30711a036fa2e0f22a3837abc7d7d7
Cristian <CristianWolfgang@gmail.com>
2023-08-17 14:26:09 -0400

    path-fixed-2

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import hamburgerBtn from "./hamburger-btn";
-import playVideo from "./play-video"
-import setActiveSection from "./set-active-section";
-import setResume from "./set-resume";
+import hamburgerBtn from "./hamburger-btn.js";
+import playVideo from "./play-video.js"
+import setActiveSection from "./set-active-section.js";
+import setResume from "./set-resume.js";
 
 document.addEventListener("DOMContentLoaded",e=>{
   playVideo();
